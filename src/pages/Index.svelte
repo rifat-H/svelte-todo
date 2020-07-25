d53f596792bd4cc8d669cf45e66c89e563992d47
Rifat Hossain <rifath.h@hotmail.com>
2020-07-26 03:25:03 +0600

    all moved to index

---
+++ src/pages/Index.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Filter from '../components/Filter.svelte';
+    import AddTask from '../components/AddTask.svelte';
+    import TaskList from '../components/TaskList.svelte';
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+
+<!-- markup (zero or more items) goes here -->
+<Filter />
+<AddTask />
+<TaskList />
