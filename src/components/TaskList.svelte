79f7cea781459353739c1f1a0f84f9aef237d99f
Rifat Hossain <rifath.h@hotmail.com>
2020-07-29 15:29:30 +0600

    filter working

--- src/components/TaskList.svelte
+++ src/components/TaskList.svelte
@@ -1,6 +1,8 @@
 <script>
     import Task from "./Task.svelte";
     import Tasks from "../stores/TaskStore.js";
+    import ShowState from "../stores/ShowStateStore.js";
+
 </script>
 
 <style>
@@ -10,7 +12,17 @@
 <!-- markup (zero or more items) goes here -->
 <div class="card mt-5 p-3">
     {#each $Tasks as task (task.id)}
-        <Task {task}/>
+
+        {#if $ShowState != 2}
+        
+            {#if task.status == $ShowState}
+                <Task {task}/>
+            {/if}
+
+            {:else}
+            <Task {task} />
+        {/if}
+
     {:else}
          <h2 class="text-center" >No Tasks Found</h2>
     {/each}
