9fd8d71c68d98417331c0bd8199b5fa05ed77680
Rifat Hossain <rifath.h@hotmail.com>
2020-07-31 20:57:48 +0600

    routify added

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,11 @@
 
 	// components
 	import Index from './pages/index.svelte';
-	import Loader from './components/Loader.svelte';
+	// import Loader from './components/Loader.svelte';
+
+	// routify router
+	import { Router } from "@sveltech/routify";
+	import { routes } from "@sveltech/routify/tmp/routes";
 
 </script>
 
@@ -12,12 +16,7 @@
 	<title>{config.APP_NAME}</title>
 </svelte:head>
 
-<div class="container">
-
-	<Loader />
-	<Index />
-</div>
-
+<Router {routes} />
 
 <style>
 	@import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css");
--- src/pages/Index.svelte
+++ src/pages/Index.svelte
@@ -7,6 +7,7 @@
     /* your styles go here */
 </style>
 
-<!-- markup (zero or more items) goes here -->
-<TopControls />
-<TaskList />
+<div class="container">
+    <TopControls />
+    <TaskList />
+</div>
